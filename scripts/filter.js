bbe784b9276ad825889ddeaa72b7e50b195a1b48
viggoStrom <viggostrom05@gmail.com>
2024-09-17 13:07:45 +0200

    restructure scripts

---
+++ scripts/filter.js
@@ -0,0 +1,7 @@
+
+const updateFilters = () => {
+    // Gets the menu container element from the DOM (this is where we will add the pizzas)
+    const menuContainer = document.getElementById("menu-items-container");
+
+
+}
---
+++ scripts/pizzas.js
@@ -0,0 +1,32 @@
+
+// Dictionary with pizza names, ingredients and prices
+const pizzas = {
+    "Margherita": {
+        "ingredients": ["Ost", "Tomatsås"],
+        "price": "80 kr"
+    },
+    "Hawaii": {
+        "ingredients": ["Ost", "Tomatsås", "Skinka", "Ananas"],
+        "price": "90 kr"
+    },
+    "Calzone": {
+        "ingredients": ["Inbakad", "Ost", "Tomatsås", "Skinka"],
+        "price": "85 kr"
+    },
+    "Capricciosa": {
+        "ingredients": ["Ost", "Tomatsås", "Skinka", "Champinjoner"],
+        "price": "90 kr"
+    },
+    "Vesuvio": {
+        "ingredients": ["Ost", "Tomatsås", "Skinka"],
+        "price": "85 kr"
+    },
+    "La Casa": {
+        "ingredients": ["Ost", "Tomatsås", "Champinjoner", "Räkor", "Skinka"],
+        "price": "95 kr"
+    },
+    "Pompeii": {
+        "ingredients": ["Ost", "Tomatsås", "Bacon", "Rödlök", "Ägg", "Curry"],
+        "price": "90 kr"
+    }
+};
