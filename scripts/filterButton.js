69a2ba6e2ef2f93b697796924e7420b05d2f4386
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2024-09-18 15:53:50 +0200

    Finished desktop animations for filtering and working on smartphone animations

--- scripts/filterButton.js
+++ scripts/filterButton.js
@@ -1,11 +1,9 @@
 document.getElementById('filter-button').addEventListener('click', function() {
     var filterContainer = document.getElementById('filter-container');
     var menuContainer = document.getElementById('menu');
-    filterContainer.classList.toggle('d-none');
-
+    var customRightBorder = document.getElementById('custom-right-border');
     // Determine the direction of the animation
     var isVisible = !filterContainer.classList.contains('d-none');
-    
     if (isVisible) {
         // If the filter container is visible, animate it out
         filterContainer.style.animationDirection = 'reverse';
@@ -14,7 +12,19 @@ document.getElementById('filter-button').addEventListener('click', function() {
         // If the filter container is hidden, animate it in
         filterContainer.style.animationDirection = 'normal';
         menuContainer.style.animationDirection = 'normal';
+        filterContainer.classList.remove('d-none');
     }
 
-    filterContainer.classList.add('slide-right');
+    filterContainer.addEventListener('animationend', function() {
+        // If the filter container was visible, hide it after the animation finishes
+        if (isVisible) {
+            filterContainer.classList.add('d-none');
+        }
+        // Remove the animation classes after the animation finishes
+        filterContainer.classList.remove('slide-animation-right');
+        menuContainer.classList.remove('slide-animation-left');
+    }, { once: true });
+
+    menuContainer.classList.add('slide-animation-left');
+    filterContainer.classList.add('slide-animation-right');
 });
