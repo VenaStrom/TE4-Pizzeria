ead6426924ffb9b6495d06525b15f605cb112c5c
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2024-09-19 09:43:45 +0200

    Finished smartphone animation for the filter

--- scripts/filterButton.js
+++ scripts/filterButton.js
@@ -1,30 +1,35 @@
-document.getElementById('filter-button').addEventListener('click', function() {
+document.getElementById('filter-button').addEventListener('click', function () {
     var filterContainer = document.getElementById('filter-container');
     var menuContainer = document.getElementById('menu');
-    var customRightBorder = document.getElementById('custom-right-border');
-    // Determine the direction of the animation
+    var isSmallScreen = window.matchMedia("(max-width: 576px)").matches;
     var isVisible = !filterContainer.classList.contains('d-none');
-    if (isVisible) {
-        // If the filter container is visible, animate it out
-        filterContainer.style.animationDirection = 'reverse';
-        menuContainer.style.animationDirection = 'reverse';
+    if (isSmallScreen) { // If the screen is small, toggle the filter container without animation
+        filterContainer.classList.toggle('d-none');
+        filterContainer.classList.remove('custom-right-border');
     } else {
-        // If the filter container is hidden, animate it in
-        filterContainer.style.animationDirection = 'normal';
-        menuContainer.style.animationDirection = 'normal';
-        filterContainer.classList.remove('d-none');
-    }
-
-    filterContainer.addEventListener('animationend', function() {
-        // If the filter container was visible, hide it after the animation finishes
-        if (isVisible) {
-            filterContainer.classList.add('d-none');
+        filterContainer.classList.add('custom-right-border');
+        if (isVisible) { // If the filter container is visible, animate it out
+            filterContainer.style.animationDirection = 'reverse';
+            menuContainer.style.animationDirection = 'reverse';
+        } else { // If the filter container is hidden, animate it in
+            filterContainer.style.animationDirection = 'normal';
+            menuContainer.style.animationDirection = 'normal';
+            filterContainer.classList.remove('d-none');
         }
-        // Remove the animation classes after the animation finishes
-        filterContainer.classList.remove('slide-animation-right');
-        menuContainer.classList.remove('slide-animation-left');
-    }, { once: true });
 
-    menuContainer.classList.add('slide-animation-left');
-    filterContainer.classList.add('slide-animation-right');
+
+        // Animation classes start the animations
+        menuContainer.classList.add('slide-animation-left');
+        filterContainer.classList.add('slide-animation-right');
+
+        // Event listener runs after the animation finishes
+        filterContainer.addEventListener('animationend', function () {
+            if (isVisible) { // If the filter container was visible, hide it after the animation finishes
+                filterContainer.classList.add('d-none');
+            }
+            // Remove the animation classes
+            filterContainer.classList.remove('slide-animation-right');
+            menuContainer.classList.remove('slide-animation-left');
+        }, { once: true });
+    }
 });
