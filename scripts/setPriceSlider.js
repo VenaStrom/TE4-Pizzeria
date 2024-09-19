b98fec410a793a85baa025fa19ab0878223881fb
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2024-09-19 10:46:43 +0200

    Fixed price slider

--- scripts/setPriceSlider.js
+++ scripts/setPriceSlider.js
@@ -1,5 +1,7 @@
 const setPriceSlider = () => {
     const priceSlider = document.getElementById("price-range");
+    const minPriceTag = document.getElementById("min-price");
+    const maxPriceTag = document.getElementById("max-price");
 
     const pizzaCopy = [...pizzas];
     pizzaCopy.sort((a, b) => {
@@ -8,6 +10,9 @@ const setPriceSlider = () => {
 
     const minPrice = parseFloat(pizzaCopy.at(0).price);
     const maxPrice = parseFloat(pizzaCopy.at(-1).price);
+
+    minPriceTag.textContent = minPrice + " kr";
+    maxPriceTag.textContent = maxPrice + " kr";
 
     priceSlider.min = minPrice;
     priceSlider.max = maxPrice;
